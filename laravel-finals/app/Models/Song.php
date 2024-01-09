2647f5941dcadf65e32fb2a38a01e6d8e177068c
rukavain <magtoto599@gmail.com>
2024-01-09 21:19:29 +0800

    laravel-final-requirement, added controller, model, and migration files

---
+++ laravel-finals/app/Models/Song.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Song extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'album',
+        'genre',
+        'producer'
+    ];
+
+    protected $dates = [
+        'created_at',
+        'updated_at',
+        'deleted_at'
+    ];
+}
